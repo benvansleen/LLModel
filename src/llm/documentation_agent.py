e0b8014d86073fb54d66ad52a2f96820274509e6
Ben Van Sleen <benvansleen@gmail.com>
2023-07-05 16:51:17 -0400

    Experimentation with inserting relevant context from Modelica documentation

--- src/llm/documentation_agent.py
+++ src/llm/documentation_agent.py
@@ -1,74 +1,104 @@
-import lancedb
-from requests import get as GET
+import os
 from dotenv import load_dotenv
+from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.document_loaders import PyPDFLoader
-from langchain.vectorstores import LanceDB
 from langchain.chat_models import ChatOpenAI
 from langchain.agents.agent_toolkits import (
     create_vectorstore_router_agent,
     VectorStoreRouterToolkit,
     VectorStoreInfo,
 )
-
+from om_embeddings import embed_fn, db
 
 load_dotenv()
 
 
-embed = OpenAIEmbeddings()
-db = lancedb.connect('/tmp/lancedb')
-
-if 'control' not in db.table_names():
-    print('Creating Python Control Library embeddings...')
-    # res = GET(
-    #     'https://python-control.readthedocs.io/_/downloads/en/0.9.2/pdf/'
-    # )
-    # with open('/tmp/control.pdf', 'wb') as f:
-    #     f.write(res.content)
-
-    loader = PyPDFLoader('/tmp/control.pdf')
-    docs = loader.load_and_split()
-    docs = docs[13:124]
-
-    table = db.create_table(
-        'control',
-        data=[{
-            'vector': embed.embed_query('dummy'),
-            'text': 'dummy',
-            'id': '1',
-        }],
-        mode='overwrite',
-    )
-    db = LanceDB.from_documents(docs, embed, connection=table)
-else:
-    table = db.open_table('control')
-    db = LanceDB(table, embed)
-
-
 vectorstore = VectorStoreInfo(
-    name='Python Control Library Documentation',
-    description='questions about control systems, systems modeling, etc',
+    name='Modelica Standard Library',
+    description='information about components, models, and example simulations for system dynamics and control. Simply use keywords to search for components or browse the Modelica Standard Library.',
     vectorstore=db,
 )
 
 llm = ChatOpenAI(
-    model_name='gpt-3.5-turbo-0613',
+    model_name='gpt-3.5-turbo-16k',
     # model_name='gpt-4-0613',
     temperature=0.0,
 )
 
-router_toolkit = VectorStoreRouterToolkit(
-    vectorstores=[vectorstore],
-    llm=llm,
+# from langchain.chains import RetrievalQA
+# from langchain.agents import initialize_agent, Tool, AgentType
+# tools = [
+#     Tool(
+#         name='Modelica Standard Library',
+#         description='information about components, models, and example simulations for system dynamics and control. Input MUST be a fully-formed question.',
+#         func=RetrievalQA.from_chain_type(
+#             llm=llm,
+#             chain_type='stuff',
+#             retriever=db.as_retriever(search_kwargs=dict(k=3)),
+#         )
+#     ),
+# ]
+# agent_executor = initialize_agent(
+#     tools,
+#     llm,
+#     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+#     verbose=True,
+#     return_intermediate_steps=True,
+# )
+
+# router_toolkit = VectorStoreRouterToolkit(
+#     vectorstores=[vectorstore],
+#     llm=llm,
+# )
+
+# agent_executor = create_vectorstore_router_agent(
+#     llm=llm,
+#     toolkit=router_toolkit,
+#     verbose=True,
+#     max_iterations=10,
+#     return_intermediate_steps=True,
+# )
+
+
+
+
+retriever = db.as_retriever(
+    # search_type='similarity_score_threshold',
+    search_kwargs=dict(
+        k=1,
+        # score_threshold=0.1,
+    ),
 )
 
-agent_executor = create_vectorstore_router_agent(
-    llm=llm,
-    toolkit=router_toolkit,
-    verbose=True,
-    max_iterations=5,
-)
+
+def modelica_documentation_lookup(search_query):
+    def format(doc):
+        source = doc.metadata['source'].replace('data/', '')
+        return f'{source}:\n{doc.page_content}'
+
+    return '\n\n'.join([
+        format(doc)
+        for doc in retriever.get_relevant_documents(search_query)
+    ])
 
 
-def consult_control_documentation(search_query):
-    return agent_executor.run(search_query)
+def consult_modelica_documentation(search_query):
+    docs = retriever.get_relevant_documents(search_query)
+    header = f'Your goal is to research the available documentation to find the most relevant information for writing a program that achieves the following objective: {search_query}. Your intended audience is an expert developer, so adjust your tone and included information accordingly. Summarize the intended purpose and key features IN A SINGLE PARAGRAPH of the following excerpt from the Modelica documentation:\n'
+    summaries = [
+        llm.predict(header + doc.page_content) for doc in docs
+    ]
+    combined = '\n\n'.join([
+        f'{doc.page_content}\nSUMMARY: {summary}'
+        for doc, summary in zip(docs, summaries)
+    ])
+    return combined
+
+    # search_query = search_query.replace('Search for', '')
+    # response = agent_executor({'input': header + search_query})
+    # import pdb; pdb.set_trace()
+    # return f"{response['intermediate_steps']}\n{response['output']}"
+    # # with io.StringIO() as buf, redirect_stdout(buf):
+    #     final_output = agent_executor(search_query)
+    #     chain = buf.getvalue()
+    # return f'{chain}\n\nRESULT:\n{final_output}\n\n'
